d96c1693daf3f2bdf977362ae5cc28a0af3d46a5
tom <tomas.b.lp@gmail.com>
2021-02-14 09:33:47 -0300

    request

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,6 +7,15 @@ def homepage():
     return """
     <h1>sup heroku</h1>
     """
+
+@app.route('/user/<username>')
+def homepage(username):
+    r = requests.get('https://github.com/tomas-b')
+    return """
+        the user is {user}.
+        <hr>
+        {html}
+        """.format(user=username, html=r.text)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)

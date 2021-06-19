53316f8986049b44c8138d26c909404c03bc7e9e
tomas-b <tomas.b.lp@gmail.com>
2021-06-19 10:35:48 -0300

    testing heroku local vars

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import os
 import requests
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
@@ -7,29 +8,14 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def homepage():
-    return """test"""
+    return ';)'
 
-# https://stackoverflow.com/questions/46490348/split-a-list-with-nn-elements-into-n-lists-with-n-elements-in-every-list
-# def chunkify(items, chunk_len):
-#     return [items[i:i+chunk_len] for i in range(0,len(items),chunk_len)]
 
-@app.route('/user/<username>')
+@app.route('/cb/<code>')
 @cross_origin()
-def userpage(username):
-
-    html = requests.get('https://github.com/%s' % username)
-    calendar = html.text.split('class="js-calendar-graph-svg">')[1].split('</svg')[0]
-    dates = []
-    
-    for box in calendar.split('data-count="')[1:]:
-        dates.append( {
-            'count': box.split('"')[0],
-            'date':  box.split('data-date="')[1].split('"')[0],
-            'level': box.split('data-level="')[1].split('"')[0]
-            } )
-
-    return jsonify(dates)
-    # return jsonify(chunkify(dates, 7))
+def githubCallback(code):
+    client_secret = os.environ.get('client_secret')
+    return client_secret
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)

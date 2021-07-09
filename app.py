56fac8990db80f0475e1da7390ad81e5054760d5
tomas-b <tomas.b.lp@gmail.com>
2021-07-08 23:08:43 -0300

    fix?

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def githubCallback(code):
     return (request.args.get('commits'))
 
 
-    data = 
+    data = {
         'client_id': os.environ.get('client_id'),
         'client_secret': os.environ.get('client_secret'),
         'code': code
